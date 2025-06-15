cbddf70f789cbd4350f46d79330e80acbd95befa
marcochippy <hello@marcoczirpek.com>
2025-06-15 13:01:34 +0200

    Component for projects (intro section)

---
+++ src/components/gallery/items/IntroProject.jsx
@@ -0,0 +1,34 @@
+function IntroProject({ details }) {
+  const toolsList = details.tools.map((tool, index) => (
+    <li className="" key={index}>
+      {tool}
+    </li>
+  ));
+
+  const summaryParagraphs = details.summary.split("\n\n");
+
+  return (
+    <>
+      <div className="flex">
+        <img src={details.opener} alt="" className="h-100" />
+        <div>
+          <p>{details.subHead}</p>
+          <h3 className="text-4xl font-bold">{details.headline}</h3>
+          <p className="text-xl font-bold">{details.time}</p>
+        </div>
+      </div>
+      <div className="flex w-[75%] mx-auto my-5">
+        <div className="">
+          {summaryParagraphs.map((paragraph, index) => (
+            <p key={index} className={index > 0 ? "mt-4 " : ""}>
+              {paragraph}
+            </p>
+          ))}
+        </div>
+        <ul className="ml-10">{toolsList}</ul>
+      </div>
+    </>
+  );
+}
+
+export default IntroProject;
--- src/components/gallery/items/ZE.jsx
+++ src/components/gallery/items/ZE.jsx
@@ -2,6 +2,20 @@ import { motion } from "framer-motion";
 import DPImage from "../../../assets/ZE/0U5A9480 Kopie.webp";
 import RImage from "../../../assets/ZE/_I9A0108.webp";
 import WIImage from "../../../assets/ZE/Wie_Is_Innenteil.webp";
+import IntroProject from "./IntroProject";
+import Opener from "../../../assets/ZE/_I9A0108.webp";
+
+const ZEDetails = {
+  headline: "Zimmermann Agentur GmbH",
+  time: "10.2022 - 09.2024",
+  subHead: "My time as Junior Graphic Designer",
+  tools: ["Photoshop", "InDesign", "Illustrator"],
+  summary: `After graduating with my Bachelor's degree in 2022, I joined Zimmermann Editorial GmbH, an agency specialising in internal communications for clients such as DHL Group, secusmart and Deutsche Bank. Working across a variety of media, including print, newsletters, mobile reporting and environmental design, has allowed me to refine my skills in both traditional and digital communication design.\n\n Working on smaller web projects made me realise how much I enjoyed the creative, challenging and technical aspects of web development. Inspired by this, I fully immersed myself in a web development bootcamp, deepening my knowledge and transitioning into more dynamic, code-driven design solutions — combining these new skills with my extensive design expertise.`,
+
+  opener: Opener,
+  icon: "/ze-feder.svg",
+  bgColor: "bg-white",
+};
 
 export default function ZE() {
   return (
@@ -11,9 +25,7 @@ export default function ZE() {
       animate={{ opacity: 1 }}
       transition={{ duration: 0.35, ease: "easeInOut" }}
       className="p-6 bg-gray-600 rounded-3xl shadow min-h-dvh">
-      <p>Time Span</p>
-      <h3 className="text-2xl font-bold">Zimmermann Editorial GmbH</h3>
-      <p>Text about my time there (Summary).</p>
+      <IntroProject details={ZEDetails} />
       <div className="grid grid-flow-col grid-rows-3 gap-4">
         <img src={DPImage} alt="" className="h-150 row-span-3 rounded-4xl" />
         <img src={RImage} alt="" className="h-70 col-span-2 rounded-4xl" />
