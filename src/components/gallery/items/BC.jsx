c2c498fe507bbd9d449bdc2509bd11a87995b9ee
marcochippy <hello@marcoczirpek.com>
2025-07-22 17:13:21 +0200

    dont know 2

--- src/components/gallery/items/BC.jsx
+++ src/components/gallery/items/BC.jsx
@@ -1,4 +1,4 @@
-import { motion } from "framer-motion";
+import { motion } from 'framer-motion';
 
 export default function BC() {
   return (
@@ -6,7 +6,7 @@ export default function BC() {
       key="project"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
-      transition={{ duration: 0.35, ease: "easeInOut" }}
+      transition={{ duration: 0.35, ease: 'easeInOut' }}
       className="p-6 bg-gray-600 rounded-lg shadow flex min-h-dvh">
       <h3 className="text-xl font-bold">BLA</h3>
       <p>Meine Design-Projekte mit Figma und Adobe XD...</p>
